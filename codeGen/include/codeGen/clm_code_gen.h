4fc251fa51dd88c0eee8b4c128bb90df5777a8b4
mapp <clowman1993@gmail.com>
2015-11-23 00:13:56 -0500

    removing old structure

--- codeGen/include/codeGen/clm_code_gen.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLM_CODE_GEN_H_
-#define CLM_CODE_GEN_H_
-
-#include "util/clm_array_list.h"
-#include "util/clm_scope.h"
-
-const char *clm_code_gen_main(ClmArrayList *statements, ClmScope *globalScope);
-
-#endif
--- typeCheck/include/typeCheck/clm_type_check.h
+++
@@ -1,9 +0,0 @@
-#ifndef CLM_TYPE_CHECK_H_
-#define CLM_TYPE_CHECK_H_
-
-#include "util/clm_array_list.h"
-#include "util/clm_scope.h"
-
-void clm_type_check_main(ClmArrayList *statements, ClmScope *globalScope);
-
-#endif
